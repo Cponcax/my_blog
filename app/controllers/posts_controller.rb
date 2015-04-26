cd2b900acf5381e176957a2bce1834c1a306ea0c
cponcax <gponcax@gmail.com>
2015-04-26 16:05:10 -0600

    add model Post & creating action create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,23 @@
 class PostsController < ApplicationController
-	def index
-		
-	end
+  def index
+    
+  end
 
-	def new
-		
-	end
+  def new
+    
+  end
 
-	def create
-		
-	end
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+      
+    end
 end
