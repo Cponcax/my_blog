721572ce4478eb47c7f640463affad5eaa2359c5
cponcax <gponcax@gmail.com>
2015-04-27 00:48:26 -0600

    creating model comment & add delete comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426220053) do
+ActiveRecord::Schema.define(version: 20150427061509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -50,4 +60,5 @@ ActiveRecord::Schema.define(version: 20150426220053) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "posts"
 end
